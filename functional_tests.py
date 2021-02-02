7e56f8d0de08a2c17d61144ca937bce6f8c7c3ed
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-02 14:28:08 -0300

    test(functional_test): Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,8 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -29,20 +31,36 @@ class NewVisitorTest(unittest.TestCase):
         # Ela percebeu que o título da página e o cabeçalho
         # mencionam listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ela é convidada a insetir um item de tarefa imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Ela digita "Comprar penas de pavão" em uma caixa de texto
         # (o hobby de Edith é fazer iscas para pescas com penas)
+        inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela tecla "enter", a página é atualizada, e agora
         # a página lista "1: Comprar penas de pavão" como um item
         # em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # Ainda continua havendo uma caixa de texto convidando-a a
         # acrescentar outro item. Ela insere "Usar penas de pavão
         # para fazer uma isca" - Edith é bem metódica)
+        self.fail('Finish the test')
 
         # A página é atualizada novamente e agora mostra os dois
         # itens em sua lista
