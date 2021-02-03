0795802ed6a02ab5af638d622ac76d067d473d97
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-03 16:10:27 -0300

    feat(lists): New URL, view and template do display lists

--- lists/tests.py
+++ lists/tests.py
@@ -22,20 +22,21 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'],
+                         '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey1')
-        Item.objects.create(text='itemey2')
+    # def test_displays_all_list_items(self):
+    #     Item.objects.create(text='itemey1')
+    #     Item.objects.create(text='itemey2')
 
-        response = self.client.get('/')
+    #     response = self.client.get('/')
 
-        self.assertIn('itemey1', response.content.decode())
-        self.assertIn('itemey2', response.content.decode())
+    #     self.assertIn('itemey1', response.content.decode())
+    #     self.assertIn('itemey2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -56,3 +57,19 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (even) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey1')
+        Item.objects.create(text='itemey2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey1')
+        self.assertContains(response, 'itemey2')
