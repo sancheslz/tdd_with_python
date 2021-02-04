5e5e473474dbe10d6537d7dd754058b155696e8a
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-04 17:44:50 -0300

    refactor(lists): Refactor templates to use a base template

--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,5 @@ from lists import views
 urlpatterns = [
     path('new/', views.new_list, name='new_list'),
     path('<list_id>/', views.view_list, name='view_list'),
-    path('<list_id>/add_item', views.add_item, name='add_item'),
+    path('<list_id>/add_item/', views.add_item, name='add_item'),
 ]
