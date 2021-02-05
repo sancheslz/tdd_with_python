bff1ec8bc529782071e3032277bdd181058d17d5
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-05 17:29:19 -0300

    refactor(settings): Adjust the Production environment

--- superlists/settings/production.py
+++ superlists/settings/production.py
@@ -1,14 +1,17 @@
 from .base import *
+from pathlib import Path
+import os
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 ALLOWED_HOSTS = [
-
+    '192.168.1.105'
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'production.sqlite3',
+        'NAME': BASE_DIR / '../database/production.sqlite3',
     }
 }
 
@@ -27,3 +30,5 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
